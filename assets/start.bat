114f36404b6d1142f49792cc604533a1f2d0f2cf
mueller <mueller@mbits.info>
2019-03-27 13:53:58 +0100

    Checkpoint commit as of 2019-03-27-13-53-58.

--- assets/start.bat
+++ assets/start.bat
@@ -1,1 +1,6 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat tensorflow
+cd src
+python gesture_control.py ../assets/hands_128_7c.hdf5 7 --run_training --image_size 128
+
 call "C:\Program Files\mRayClient\bin\mRayClient.exe" --hotkey
+CMD
