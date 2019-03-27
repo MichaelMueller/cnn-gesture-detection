7c7f8a10e7a952712c34d8b7aa99550db3b16195
mueller <mueller@mbits.info>
2019-03-27 07:33:45 +0100

    changes

---
+++ src/cnn-gesture-detection.py
@@ -0,0 +1,10 @@
+
+# load config file
+
+# run video
+
+# for each frame:
+#   segment hand
+#   if hands are found:
+#       classify mask
+#       use keymap to create a key event based on the mask
